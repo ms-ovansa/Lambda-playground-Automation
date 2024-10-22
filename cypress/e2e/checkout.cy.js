592df45567eaafff4092484517e333f449bb9001
saadat <saadeebrahim001@gmail.com>
2024-10-22 15:47:37 +0100

    Test GitHub Actions workflow

--- cypress/e2e/checkout.cy.js
+++ cypress/e2e/checkout.cy.js
@@ -2,7 +2,7 @@
 import CheckoutPage from '../support/pages/checkoutPage'; 
 import UserPage from '../support/pages/userPage'; 
 
-describe('Lambda Playground Checkout Flow', () => {
+describe('Lambda Playground Checkout Test', () => {
   const checkoutPage = new CheckoutPage();
   const userPage = new UserPage(); 
   beforeEach(() => {
@@ -20,28 +20,13 @@ describe('Lambda Playground Checkout Flow', () => {
       checkoutPage.addToCart(); 
       checkoutPage.proceedToCheckout();
       userPage.fillUserData();
-
-    // cy.get('#input-payment-firstname').type(userData.first_name);
-    // cy.get('#input-payment-lastname').type(userData.last_name);
-    //   cy.get('#input-payment-email').type(userData.email);
-    //   cy.get('#input-payment-telephone').type(userData.phone);
-    //   cy.get('#input-payment-password').type(userData.password);
-    //   cy.get('#input-payment-confirm').type(userData.password);
-    //   cy.get('#input-payment-company').type(userData.company);
-    //    cy.get('#input-payment-address-1').type(userData.address);
-    //   cy.get('#input-payment-address-2').type(userData.address2);
-    //   cy.get('#input-payment-city').type(userData.city);
-    //   cy.get('#input-payment-postcode').type(userData.postcode);
-    //   cy.get('#input-payment-country').select(userData.country);
-    //   cy.get('#input-payment-zone').select(userData.state);
-
       checkoutPage.agreeToTerms(); 
       checkoutPage.clickNewsLetter();
       checkoutPage.submitCheckout(); 
 
       
 
-cy.contains("Confirm Order").should('be.visible');
+cy.contains("Confirm Orders").should('be.visible');
 checkoutPage.confirmCheckout();
 cy.contains("Your order has been placed!").should('be.visible');
 
